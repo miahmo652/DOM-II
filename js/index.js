60097c3cb821f9c8365ce0d3d29d6b29f691e0da
miahmo652 <miahmo652@users.noreply.github.com>
2020-04-14 19:36:04 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,30 +1,35 @@
 // Your code goes here
 
-// Images animation
 
+
+//mouseenter event
 const images = document.querySelectorAll("img");
 images.forEach((image) => {
     image.addEventListener("mouseenter", (event) => {
         image.style.transform = "scale(1.4)";
         image.style.transition = "all 1s";
     });
+    // mouseleave event
     image.addEventListener("mouseleave", (event) => {
         image.style.transform = "scale(1)";
     });
 });
 
-// h4 rotation
+// mousedown event
 
 const links = document.querySelectorAll(".content-pick h4");
 links.forEach((link) => {
     link.addEventListener("mousedown", (event) => {
-        event.target.style.transform = "rotate(360deg)";
-        event.target.style.transition = "transform 1s";
+            event.target.style.transform = "rotate(180deg)";
+            event.target.style.transition = "transform 1s";
 
+        }) // mouseup event
+    link.addEventListener("mouseup", (event) => {
+        event.target.style.transform = "rotate(360deg)";
     })
 })
 
-// Background color changes
+//  wheel event 
 
 const bodys = document.querySelector("body");
 bodys.addEventListener("wheel", function() {
@@ -37,6 +42,8 @@ bodys.addEventListener("wheel", function() {
 
 })
 
+// click event
+
 const butt = document.querySelectorAll(".btn");
 butt.forEach((but) => {
     but.addEventListener("click", (event) => {
@@ -44,3 +51,35 @@ butt.forEach((but) => {
         event.target.style.transition = "transform 1s";
     })
 })
+
+// keydown event
+const kdown = document.querySelectorAll("img");
+kdown.forEach((image) => {
+    image.addEventListener("dblclick", (event) => {
+        image.style.transform = "scale(2)";
+        image.style.transition = "all 1s";
+    });
+});
+
+
+// resizing event
+const resiz = document.querySelector("body");
+resiz.addEventListener('resize', (event) => {
+    event.style.backgroundColor = "pink";
+})
+
+
+//focus
+
+const foc = document.querySelector(".nav");
+foc.addEventListener('focus', (event) => {
+    event.target.style.background = 'orange';
+}, true);
+
+
+//contextmenu
+noContext = document.querySelector('.intro h2');
+
+noContext.addEventListener('contextmenu', event => {
+    event.preventDefault();
+});
